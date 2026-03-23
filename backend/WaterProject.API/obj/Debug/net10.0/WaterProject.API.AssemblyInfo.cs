5da68599b67897174b3ec13e3bbd6a6aa1b70510
Hector Casablanca <jeremiahedu2019@gmail.com>
2026-03-22 19:35:33 -0600

    Getting stuff up to the repo after videos

--- backend/WaterProject.API/obj/Debug/net10.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net10.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b34e86dfcc3447383b5ec2ba017c6e35f1f2b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c1f36905a1ebdb5ca05b711611f5690f803fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
-import type { Project } from './types/Project';
+import type { Project } from '../types/Project';
+import { useNavigate } from "react-router-dom";
 
 function ProjectList({selectedCategories}: {selectedCategories: string[]}){
 const[projects, setProjects] = useState<Project[]>([]);
@@ -7,6 +8,7 @@ const [pageSize, setPageSize] = useState<number>(10);
 const [pageNum, setPageNum] = useState<number>(1);
 const [totalItems, setTotalItems] = useState<number>(0);
 const [totalPages, setTotalPages] = useState<number>(0);
+const navigate =useNavigate();
 
 useEffect(() => {
     const fetchProjects = async() => {
@@ -28,16 +30,19 @@ useEffect(() => {
     return(
         <>
         {projects.map((p) =>
-        <div id="projectCard" className="card" key={p.projectID}>
+        <div id="projectCard" className="card" key={p.projectId}>
             <h3 className="card-title">{p.projectName}</h3>
                 <div className="card-body">
                    <ul className="list-unstyled">
-                <li><strong>Project Type:</strong> {p.projectType}</li>
-                <li><strong>RegionalProgram</strong>: {p.projectRegionalProgram}</li>
-                <li><strong>Impact:</strong> {p.projectImpact} Individuals Served</li>
-                <li><strong>Project Phase: </strong> {p.projectPhase}</li>
-                <li><strong>Project Status: </strong> {p.projectFunctionalityStatus}</li>
+                        <li><strong>Project Type:</strong> {p.projectType}</li>
+                        <li><strong>RegionalProgram</strong>: {p.projectRegionalProgram}</li>
+                        <li><strong>Impact:</strong> {p.projectImpact} Individuals Served</li>
+                        <li><strong>Project Phase: </strong> {p.projectPhase}</li>
+                        <li><strong>Project Status: </strong> {p.projectFunctionalityStatus}</li>
                     </ul> 
+
+                    <button className='btn btn-success' onClick={() => navigate(`/donate/${p.projectName} /${p.projectId}`)}>Donate</button>
+
                 </div>
             
         </div>
